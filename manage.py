bd333ee19ab63e33a89895cbe2db6e8f9781376b
Daven Quinn <dev@davenquinn.com>
2014-07-03 03:33:43 -0700

    Most minimal flask app imaginable.
    Actually that's not really true; it does a lot. Like serve static files!

--- manage.py
+++ manage.py
@@ -3,20 +3,17 @@
 from __future__ import division, print_function
 
 from flask.ext.script import Manager, Server
-from flask.ext.migrate import MigrateCommand
 
-from samples.app import app
-from samples.database import db, models
+from samples.application import app, db
 
 manager = Manager(app)
 
 server = Server(host='0.0.0.0', port=8000)
 manager.add_command("serve", server)
-manager.add_command("database", MigrateCommand)
 
 @manager.shell
 def make_context():
-	return dict(app=app,db=db,models=models)
+	return dict(app=app,db=db)
 
 if __name__ == "__main__":
     manager.run()
---
+++ samples/application/base.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+from flask import Flask, Blueprint
+from ..config import DATA_DIR
+
+data = Blueprint('static', __name__,
+    static_folder=STATIC_DIR,
+    static_url_path="")
+
+class Application(Flask):
+    def __init__(self, *args,**kwargs):
+        defaults = dict(
+            static_url_path="",
+            static_folder="static")
+        Flask.__init__(self, *args,**kwargs)
+        self.config.from_object("samples.config")
+        self.register_blueprint(data, url_prefix="/static")
