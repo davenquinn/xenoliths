5e03c1ac19aa4c9de0e7a8d8371047943c4a65b5
Daven Quinn <dev@davenquinn.com>
2014-07-03 03:07:11 -0700

    Made manager something that Flask would like.

--- manage.py
+++ manage.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
-import os
-import sys
+
+from __future__ import division, print_function
+
+from flask.ext.script import Manager, Server
+from flask.ext.migrate import MigrateCommand
+
+from samples.app import app
+from samples.database import db, models
+
+manager = Manager(app)
+
+server = Server(host='0.0.0.0', port=8000)
+manager.add_command("serve", server)
+manager.add_command("database", MigrateCommand)
+
+@manager.shell
+def make_context():
+	return dict(app=app,db=db,models=models)
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "samples.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+    manager.run()
