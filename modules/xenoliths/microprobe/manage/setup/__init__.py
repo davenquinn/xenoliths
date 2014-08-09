8e4983cc90e5f48ad70a1af949890419134defac
Daven Quinn <dev@davenquinn.com>
2014-08-08 17:21:59 -0700

    Finished a new, simpler, data import specification.

--- modules/xenoliths/microprobe/manage/setup/__init__.py
+++ modules/xenoliths/microprobe/manage/setup/__init__.py
@@ -1,72 +1,56 @@
 from click import echo
-import numpy as N
 import os
-from geoalchemy2.shape import from_shape
+from geoalchemy2.elements import WKTElement
 import json
-import re
-from pathlib import Path
-from datetime import datetime
-from pandas import read_table
 
+from .file_handler import get_data
 from ....application import app, db
 from ...models import Sample, Point
-from .array import Array
 
 def write_json():
-	path = os.path.join(app.config.get("DATA_DIR"),"data.json")
-	data = dict(
-		type="FeatureCollection",
-		features=map(lambda o: o.serialize(), Point.query.all()))
-	with open(path, "w") as f:
-		json.dump(data, f)
+    path = os.path.join(app.config.get("DATA_DIR"),"data.json")
+    data = dict(
+        type="FeatureCollection",
+        features=map(lambda o: o.serialize(), Point.query.all()))
+    with open(path, "w") as f:
+        json.dump(data, f)
 
-def import_sample(sample_name):
-	arr = Array(sample_name+".dat")
+def oxide_weights(row):
+    ox = {k:row[k] for k in app.config.get("OXIDES")}
+    ox["Total"] = sum(ox.values())
+    return ox
 
-	arr.transform_coordinates(sample_name+"_affine.txt")
+def geometry(row):
+    xy = row["X-POS"],row["Y-POS"]
+    return WKTElement("POINT({0} {1})".format(*xy))
 
-	sample = Sample.get_or_create(id=sample_name)
-
-	for rec in arr.each():
-		point = Point.get_or_create(
-			line_number=int(rec.id),
-			sample=sample)
-		point.geometry = from_shape(rec.geometry)
-		point.oxides = rec.oxide_weights()
-		point.errors = rec.errors()
-		point.derived_data()
-	db.session.commit()
-
-
-sample_regex = re.compile(r"(CK-\S+)")
-date_regex = re.compile(r"(\d\d-\d\d-\d\d)")
-
-def parse_filename(path):
-	name = path.stem.replace("_"," ")
-	m = sample_regex.search(name)
-	date = date_regex.search(name).group()
-	return dict(
-		sample_id = m.group(),
-		group = name[m.end()+1:]
-		date = datetime.strptime(date, "%m-%d-%y"))
+def create_samples(data):
+    for val in data["sample_id"].unique():
+        sample = Sample.get_or_create(id=val)
+        db.session.add(sample)
+        yield val, sample
+    db.session.commit()
 
 def import_all():
-	data_dir = Path(app.config.get("RAW_DATA"))/"Probe"/"datafiles"
-	files = data_dir.glob("*.dat")
+    db.drop_all()
+    db.create_all()
 
-	for path in files:
-		file_data = parse_filename(path)
-		data = read_table(str(path))
+    data = get_data(app.config.get("RAW_DATA"))
 
-		s_id = file_data.pop("sample_id")
-		assert s_id in app.config.get("SAMPLES")
-		sample = Sample.get_or_create(id=s_id)
+    samples = {k:v for k,v in create_samples(data)}
+
+    for i,row in data.iterrows():
+        print(i)
+        sample = samples[row["sample_id"]]
+        point = Point.get_or_create(
+            line_number=row["LINE"],
+            sample=sample)
+        point.geometry = geometry(row)
+        point.oxides = oxide_weights(row)
+        #point.errors = {k:row[k+" %ERR"] for k in app.config.get("CATIONS")}
+        point.derived_data()
+        db.session.add(point)
+    db.session.commit()
 
 
-
-	import IPython; IPython.embed()
-
-
-
-if __name__ == "__main__":
-	import_all()
+    write_json()
