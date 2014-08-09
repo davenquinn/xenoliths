78dd5fd68002428e726078f27f6b64cda7533bb6
Daven Quinn <dev@davenquinn.com>
2014-08-09 16:08:31 -0700

    Added a basic database backup command.

--- modules/xenoliths/database/manage.py
+++ modules/xenoliths/database/manage.py
@@ -1,3 +1,6 @@
+import click
+import arrow
+
 from flask.ext.script import Manager
 from subprocess import call
 from pathlib import Path
@@ -6,8 +9,16 @@ from ..application import app
 
 MigrateCommand = Manager()
 
+def run(command):
+    click.secho(command, fg="cyan")
+    call(command, shell=True)
+
 @MigrateCommand.command
 def backup():
+    click.echo("Backing up database...")
     db_name, data_dir = map(app.config.get,("DB_NAME","DATA_DIR"))
 
-    import IPython; IPython.embed()
+    fn = arrow.now().format('YYYY-MM-DD_HH:mm:ss')+".sql"
+    path = Path(data_dir)/"backups"/fn
+    run("pg_dump -Fc {0} > {1}".format(db_name, path))
+    click.secho("Success!", fg="green")
