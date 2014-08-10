1f3ad619ee0a4da56ce832f76ba0480e891f3bed
Daven Quinn <dev@davenquinn.com>
2014-08-10 05:24:50 -0700

    Configuration for refactor with browserifY-FTW

---
+++ frontend/scripts/require-config.js
@@ -0,0 +1,30 @@
+require.config({
+    shim: {
+        "openlayers": {
+            exports: 'OpenLayers'
+        },
+        "jquery.bootstrap": {
+            deps: ["jquery"]
+        },
+        "jquery.slider": {
+            deps: ["jquery"]
+        },
+        "jquery.switch": {
+            deps: ["jquery"]
+        }
+    },
+    urlArgs: "bust=" +  (new Date()).getTime(),
+    paths: {
+        "classy": "lib/classy/classy",
+        "jquery": null,
+        "underscore": null,
+        "backbone": null,
+        "handlebars": null,
+        "openlayers": "lib/openlayers/OpenLayers",
+        "d3": null,
+        "jquery.bootstrap": "lib/bootstrap/dist/js/bootstrap",
+        "jquery.slider": "lib/jquery-simple-slider/js/simple-slider",
+        "jquery.switch": "lib/bootstrap-switch/static/js/bootstrap-switch",
+        "text" : "lib/requirejs-text/text",
+    }
+});
