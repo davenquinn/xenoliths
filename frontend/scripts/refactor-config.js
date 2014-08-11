96d1fd135ffc006ce942c1a24eb86073529b9c7d
Daven Quinn <dev@davenquinn.com>
2014-08-11 03:29:33 -0700

    Removed a bunch of scripts left over from building.

--- frontend/scripts/refactor-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    quote           :  '\''         // '\'' or '"'
-  , style           :  'var'        // 'var', 'comma', 'comma-first'
-  , indent          :  4            // the tab size used in your project
-  , directoryFilter :  null         // not supported yet
-  , fileFilter      :  '.js'        // the extension of the file to upgrade
-  , dryrun          :  false         // true|false if true no changes will be written to upgraded files
-};
