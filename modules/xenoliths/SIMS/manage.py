34031c50e9feea5b1c60eac51761cd7a314e6dad
Daven Quinn <dev@davenquinn.com>
2014-09-09 00:15:46 -0700

    Updated SIMS models and import scripts.

--- modules/xenoliths/SIMS/manage.py
+++ modules/xenoliths/SIMS/manage.py
@@ -1,4 +1,61 @@
 from flask.ext.script import Manager
+from click import echo
+from pathlib import Path
+
+import numpy as N
+
+from ..models import Sample
+from .models import Measurement, Datum, db
 from ..application import app
 
 SIMSCommand = Manager(usage="Command to manage SIMS data")
+
+def load_data(file):
+    dtype = [("el", str, 2), ("abundance", float), ("err", float)]
+    return N.loadtxt(str(file), skiprows=2, dtype=dtype)
+
+def import_measurement(mineral, raw,norm):
+    def clean_filename(name):
+        """ Returns sample id and cleaned name from measurement"""
+        sid, end = tuple(raw.stem.split("_",1))
+        return sid, end.split(".")[0]
+
+    assert raw.stem == norm.stem
+    sample_id, name = clean_filename(raw.stem)
+    sample = Sample.query.get(sample_id)
+    if not sample: return # We're not importing standards
+    meas = Measurement(
+        name = name,
+        sample = sample,
+        mineral = app.config["MINERALS"][mineral])
+    echo(meas)
+    db.session.add(meas)
+
+    raw, norm = tuple(map(load_data,(raw,norm)))
+    for rd, nd in zip(raw, norm):
+        assert rd["el"] == nd["el"]
+        d = Datum(
+            measurement = meas,
+            raw_ppm = rd["abundance"],
+            raw_std = rd["err"],
+            norm_ppm = nd["abundance"],
+            norm_std = nd["err"])
+        d.element = rd["el"]
+        db.session.add(d)
+
+
+
+@SIMSCommand.command
+def init():
+    """ Import SIMS data into application."""
+    Datum.query.delete()
+    Measurement.query.delete()
+    db.session.commit()
+    
+    data_dir = Path(app.config.get("RAW_DATA"))/"SIMS"
+    for mineral in ["cpx", "opx"]:
+        directory = data_dir/mineral
+        raw = list(directory.glob("*.ppm"))
+        nrm = list(directory.glob("*.nrm"))
+        [import_measurement(mineral,*i) for i in zip(raw,nrm)]
+        db.session.commit()
--- modules/xenoliths/SIMS/models.py
+++ modules/xenoliths/SIMS/models.py
@@ -1,3 +1,6 @@
+from periodictable import elements
+from sqlalchemy.ext.hybrid import hybrid_property
+
 from ..core.models.base import BaseModel, db
 from ..database.util import ChoiceType
 from ..config import MINERALS
@@ -8,22 +11,35 @@ class Datum(BaseModel):
         db.Integer,
         db.ForeignKey('sims_measurement.id'),
         primary_key=True)
-    element = db.Column(db.Integer, primary_key=True)
+    _element = db.Column("element", db.Integer, primary_key=True)
     norm_ppm = db.Column(db.Float)
     norm_std = db.Column(db.Float)
     raw_ppm = db.Column(db.Float)
     raw_std = db.Column(db.Float)
 
+    @hybrid_property
+    def element(self):
+        return elements(self._element).symbol
+
+    @element.setter
+    def element(self, value):
+        self._element = getattr(elements,value).number
+
 class Measurement(BaseModel):
     __tablename__ = "sims_measurement"
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
     sample_id = db.Column(
         db.String(64),
         db.ForeignKey("sample.id"))
     description = db.Column(db.String(1024))
     mineral = db.Column(ChoiceType(MINERALS))
 
-    elements = db.relationship("sims_datum",
+    data = db.relationship(Datum,
         backref="measurement",
-        order_by="sims_datum.element")
-    sample = db.relationship("sample", backref="sims_measurements")
+        order_by=Datum._element)
+    sample = db.relationship("Sample", backref="sims_measurements")
+
+    def __repr__(self):
+        return "SIMS: {0} {1} ({2})".format(
+            self.sample.id, self.name, self.mineral)
