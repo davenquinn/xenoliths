23636623a6c6629d21d07b834b650e3c09bc6ff7
Daven Quinn <dev@davenquinn.com>
2014-09-08 19:48:39 -0700

    Added a separate file for manager

---
+++ modules/xenoliths/manage.py
@@ -0,0 +1,35 @@
+from __future__ import division, print_function
+
+from flask.ext.script import Manager, Server
+
+from xenoliths.application import app, db
+from xenoliths.microprobe.manage.setup import import_all
+from xenoliths.microprobe import models
+from xenoliths.database.manage import MigrateCommand
+from xenoliths.thermometry.command import TemperatureCommand
+from xenoliths.heatflow import HeatFlowCommand
+from xenoliths.SIMS import SIMSCommand
+
+manager = Manager(app)
+
+server = Server(host='0.0.0.0', port=8000)
+manager.add_command("serve", server)
+manager.add_command("db", MigrateCommand)
+manager.add_command("sims", SIMSCommand)
+manager.add_command("temperature", TemperatureCommand)
+manager.add_command("heat-flow", HeatFlowCommand)
+
+@manager.shell
+def make_context():
+    return dict(app=app,db=db,models=models)
+
+@manager.command
+def setup(hard=False):
+    with app.app_context():
+        if hard: db.drop_all()
+        db.create_all()
+        import_all()
+
+
+if __name__ == "__main__":
+    manager.run()
