232b01b9677935f17aff6b325568f4a43829b387
Daven Quinn <dev@davenquinn.com>
2015-01-30 13:04:37 -0800

    Also query by SIMS datum

--- modules/xenoliths/models.py
+++ modules/xenoliths/models.py
@@ -1,3 +1,3 @@
 from .core.models import Sample
 from .microprobe.models import ProbeMeasurement, ProbeDatum, Tag
-from .SIMS.models import SIMSMeasurement
+from .SIMS.models import SIMSMeasurement, SIMSDatum
