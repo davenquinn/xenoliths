1fc900661f99a23c8d461847afd401a601512c1d
Daven Quinn <dev@davenquinn.com>
2015-04-13 04:17:14 -0700

    Created column for spot-size

--- modules/xenoliths/microprobe/models/__init__.py
+++ modules/xenoliths/microprobe/models/__init__.py
@@ -91,6 +91,7 @@ class ProbeMeasurement(BaseModel):
     oxide_total = db.Column(db.Float)
     mg_number = db.Column(db.Float)
     cr_number = db.Column(db.Float)
+    spot_size = db.Column(db.Float)
 
     errors = db.Column(JSON)
     transforms = db.Column(JSON)
