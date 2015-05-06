35e8a87c0e433b241959d73faeddb4bf223b8704
Daven Quinn <dev@davenquinn.com>
2015-05-06 16:25:15 -0700

    Excised unused parts of heatflow module

--- scenario.py
+++
@@ -1,8 +0,0 @@
-class ModelScenario(object):
-    def __init__(self, name, **kwargs):
-        self.name = name
-        for k,arg in kwargs.items():
-            setattr(self,k,arg)
-
-    def __call__(self):
-        pass
