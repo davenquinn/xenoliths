d9f17c6ef6d16cef038495c9ba1d275db402653c
Daven Quinn <dev@davenquinn.com>
2015-11-17 17:14:53 -0800

    Updated isotopes table generation

--- generate-table.py
+++ generate-table.py
@@ -1,8 +1,21 @@
+from __future__ import division
 from pandas import DataFrame
 from paper.text import tex_renderer, write_file
+from uncertainties import ufloat
 
-from IPython import embed
+def build_data():
+    data = DataFrame.from_csv("isotope-data.tsv",sep='\t')
+    for row in data.iterrows():
+        d = row[1].to_dict()
+        d['id'] = row[0]
+        for i,s in [('143Nd/144Nd(0)','std err%'),
+                  ('87Sr/86Sr(0)','std err%.1')]:
+            s = d.pop(s)
+            d[i] = ufloat(d[i],s/100*d[i])
+        yield d
 
-data = DataFrame.from_csv("isotope-data.tsv",sep='\t')
+text = (tex_renderer
+    .get_template("isotopes.tex")
+    .render(samples=build_data()))
 
-write_file("build/isotopes-table.tex", data.to_latex())
+write_file("build/isotopes-table.tex", text)
