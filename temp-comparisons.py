5570ebbf74075a43d29066bf7ce08e410eb95ea0
Daven Quinn <dev@davenquinn.com>
2015-12-07 22:33:10 -0800

    Changed axis scales for temperature comparisons

--- temp-comparisons.py
+++ temp-comparisons.py
@@ -2,10 +2,11 @@
 
 from __future__ import division, print_function
 import os
-
+import matplotlib
 import matplotlib.pyplot as P
 import numpy as N
 from pickle import dump, load
+from chroma import Color
 
 from xenoliths import app
 from xenoliths.thermometry.results import xenoliths, sample_temperatures
@@ -14,7 +15,7 @@ cache = "build/comparison-data.pickle"
 
 def create_data():
     with app.app_context():
-        data = [sample_temperatures(s, distinct=False)
+        data = [sample_temperatures(s, distinct=min)
            for s in xenoliths()]
     with open(cache,"w") as f:
         dump(data,f)
@@ -26,12 +27,20 @@ try:
 except IOError:
     data = create_data()
 
-thermometers = [
-    {"id":"ta98","name": "TA98"},
-    {"id":"bkn", "name": r"T$_{BKN}$"},
-    {"id": "ca_opx", "name": "Ca-in-Opx"},
-    {"id": "ca_opx_corr", "name": "Ca-in-OPX (Nimis and Grutter, 2010 correction)"}
-    ]
+font = {'family' : 'Helvetica Neue',
+        'weight' : 'light',
+        'size'   : 10}
+
+matplotlib.rc('font', **font)
+
+thermometers = {
+    "ta98":{"name": "TA98"},
+    "bkn":{"name": r"T$_{BKN}$"},
+    "ca_opx":{"name": "Ca-in-Opx"},
+    "ca_opx_corr":{"name": "Ca-in-OPX (corrected)"} # Nimis and Grutter, 2010 
+    }
+
+plots = ('bkn','ca_opx_corr')
 
 props = {
     "core": {
@@ -46,42 +55,41 @@ props = {
 }
 annotate_props = dict(xytext=(5,-5), textcoords='offset points', ha='left', va='center')
 
-def plot_separated(th1,th2):
-    names = [th["name"] for th in [th1,th2]]
-    ids = [th["id"] for th in [th1,th2]]
-    fig = P.figure()
-    ax = fig.add_subplot(111)
+fig, axes = P.subplots(2,1,figsize=(4,6),sharex=True)
+fig.subplots_adjust(hspace=0.05)
+
+th1 = 'ta98'
+for th2,ax in zip(plots,axes):
+    names = [thermometers[th]["name"] for th in [th1,th2]]
+    ids = [th1,th2]
 
     for sample in data:
         name = sample["id"]
 
         for a_loc in ["core", "rim"]:
             values = [sample[a_loc][i]["sep"] for i in ids]
-            print(len(values[0]),len(values[1]))
+            color = Color(sample['color'])
             if a_loc == "core":
-                popts = dict(color=sample['color'])
+                color.alpha = 0.5
+                popts = dict(color=color.rgb)
             elif a_loc == "rim":
-                popts = dict(edgecolor=sample['color'], color="#ffffff")
-            try:
-                ax.scatter(values[0], values[1], marker="o", s=20, alpha=0.7, **popts)
-            except ValueError as err:
-                print(err)
+                color.alpha = 0.2
+                f = Color("#ffffff")
+                f.alpha = 0
+                popts = dict(edgecolor=color.rgb, color=f.rgb)
+            ax.scatter(values[0], values[1], marker="o", s=20, **popts)
 
-
-    ax.set_xlim([900,1150])
-    ax.set_ylim([900,1150])
-    ax.set_xlabel(u"{0} \u00b0C".format(names[0]))
     ax.set_ylabel(u"{0} \u00b0C".format(names[1]))
     ax.autoscale(False)
+    ax.set_xlim([900,1200])
+    if th2 == 'ca_opx_corr':
+        ax.set_ylim([925,1225])
+    else:
+        ax.set_ylim([975,1275])
     ax.plot([0,1800],[0,1800],color="#cccccc", zorder=-20)
 
+ax.set_xlabel(u"{0} \u00b0C".format(names[0]))
 
-    fig.suptitle("{0} vs. {1} (Separated, P = {2:.1f} GPa)".format(names[1], names[0],1.5))
-    path = os.path.join("build", "{0}-{1}.separate.pdf".format(ids[1], ids[0]))
-    fig.savefig(path, bbox_inches="tight")
-
-for th1 in thermometers:
-    for th2 in thermometers:
-        if th1 == th2: continue
-        plot_separated(th1,th2)
+path = os.path.join("build", "thermometer-comparisons.pdf")
+fig.savefig(path, bbox_inches="tight")
 
