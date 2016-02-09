38fa95c7f19552f214a9ca13e6810c28a4134f0b
Daven Quinn <dev@davenquinn.com>
2016-02-08 18:20:58 -0800

    New way of representing Dree

--- pyx-dree.py
+++ pyx-dree.py
@@ -1,14 +1,19 @@
 # -*- coding:utf-8 -*-
 
+from __future__ import print_function, division
+
+import numpy as N
 from matplotlib.pyplot import subplots
 from xenoliths import app
 from xenoliths.models import Sample
 from xenoliths.thermometry.rare_earth import (
-    ree_pyroxene, regress, temperature)
+    ree_pyroxene, regress, temperature, rare_earths)
 
 def plot_DREE(ax, sample, annotate=True):
     X,Y = ree_pyroxene(sample, 1.5) # Pressure in GPa
     res = regress(X,Y)
+    fitted_coeff = res.params[0]
+
     T = temperature(res)
 
     title = u"{id}: {n:.0f}±{s:.0f} °C"\
@@ -16,21 +21,21 @@ def plot_DREE(ax, sample, annotate=True):
             id=sample.id,
             n=T.n,
             s=T.s)
-    ax.plot(X,Y, "o")
-    ax.plot(res.X,res.fittedvalues,"-")
+
+    xv = N.arange(len(X))
+    ax.plot(xv,Y/X-273.15, "-", color=sample.color, linewidth=2, zorder=5)
+    T = res.params[0]-273.15 * N.ones_like(xv)
+    ax.plot(xv,T,"-",color=sample.color)
+
 
     res = regress(X,Y,hree_only=True)
-    ax.plot(res.X,res.fittedvalues,"-",color="#aaaaaa")
+    T = res.params[0]-273.15 * N.ones_like(xv)
+    ax.plot(xv,T,":",color=sample.color)
 
-    if not annotate:
-        return ax
-    for x,y,t in zip(X,Y,rare_earths):
-        ax.annotate(t, (x,y),
-                xytext=(5,5),
-                textcoords="offset points")
+    return ax
 
 with app.app_context():
-    fig, axs = subplots(2,3,
+    fig, ax = subplots(
             figsize=(6.5,5),
             sharey=True, sharex=True)
     samples = (Sample.query
@@ -38,10 +43,12 @@ with app.app_context():
         .order_by(Sample.id)
         .all())
 
-    for ax, sample in zip(axs.flatten(), samples):
+    for sample in samples:
         ax = plot_DREE(ax, sample, annotate=False)
-        ax.set_ylabel(r"B")
-        ax.set_xlabel(r"$D_{ree}$")
 
-    fig.subplots_adjust(hspace=0.1,wspace=0.05)
+    ax.set_ylabel(u"T (\u00b0C)")
+
+    ax.xaxis.set_ticks(N.arange(len(rare_earths)))
+    ax.xaxis.set_ticklabels(rare_earths)
+
     fig.savefig("build/pyx-dree.pdf",bbox_inches="tight")
