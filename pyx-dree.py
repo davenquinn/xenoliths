c44b787ff636d68164f6087d2433d933a8d6c16e
Daven Quinn <dev@davenquinn.com>
2016-02-25 03:09:39 -0800

    Added a way to throw out different REEs

--- pyx-dree.py
+++ pyx-dree.py
@@ -3,7 +3,9 @@
 from __future__ import print_function, division
 
 import numpy as N
-from matplotlib.pyplot import subplots
+from matplotlib.gridspec import GridSpec
+from matplotlib.pyplot import figure
+from scipy.stats import norm
 from xenoliths import app
 from xenoliths.models import Sample
 from xenoliths.thermometry.rare_earth import (
@@ -11,9 +13,14 @@ from xenoliths.thermometry.rare_earth import (
 
 xv = N.arange(len(rare_earths))
 
+with open('ree-disequilibrium.txt') as f:
+    lines = (d.split() for d in f.readlines())
+    for_removal = {d.pop(0):d for d in lines}
+
 # Leave space for Praseodymium
 # Pr_ix = 4
 # xv[Pr_ix:] += 1
+
 
 def plot_uncertain(ax, x,y, **kwargs):
     u = N.array([m.n for m in y])
@@ -26,11 +33,15 @@ def plot_uncertain(ax, x,y, **kwargs):
         alpha=0.2)
     ax.plot(x,u, color=color, **kwargs)
 
-def plot_DREE(ax, sample, annotate=True, uncertainties=True):
+def plot_DREE(axes, sample, annotate=True, uncertainties=True):
+    ax,ax1 = axes
     X,Y = ree_pyroxene(sample, 1.5) # Pressure in GPa
-    res = regress(X,Y)
-    fitted_coeff = res.params[0]
 
+    to_remove = for_removal[sample.id]
+    # Index of elements to remove from regression
+    ix = N.in1d(rare_earths,to_remove)
+    res = regress(X[~ix],Y[~ix])
+    fitted_coeff = res.params[0]
     T = temperature(res)
 
     title = u"{id}: {n:.0f}±{s:.0f} °C"\
@@ -40,35 +51,46 @@ def plot_DREE(ax, sample, annotate=True, uncertainties=True):
             s=T.s)
 
     y = Y/X-273.15
+
     kwargs = dict(
-        color=sample.color)
+        color=sample.color,
+        linewidth=1)
+
+    n = N.array([m.n for m in y])
     if uncertainties:
         plot_uncertain(ax,xv,y,**kwargs)
     else:
-        n = N.array([m.n for m in y])
         ax.plot(xv,n,'-',**kwargs)
-    T = res.params[0]-273.15 * N.ones_like(xv)
 
-    ax.plot(xv,T,"-",color=sample.color, linewidth=0.5, zorder=-5)
+    # Plot filled and open circles
+    kws = dict(edgecolor=sample.color, marker='o', zorder=10)
+    ax.scatter(xv[ix],n[ix], color='#ffffff', **kws)
+    ax.scatter(xv[~ix],n[~ix],color=sample.color,**kws)
+
+    ax.axhline(y=T.n, label=sample.id, color=sample.color)
 
 
-    res = regress(X,Y,hree_only=True)
-    T = res.params[0]-273.15 * N.ones_like(xv)
-    ax.plot(xv,T,":",color=sample.color, linewidth=0.5, zorder=-5)
+    # Plot probability density function
+    s = res.bse[0]
+    offs = fitted_coeff-273.15
+    x = N.linspace(-3*s,3*s,100)
+    P = norm.pdf(x,0,s)
+    ax1.plot(P,x+offs, color=sample.color)
 
-    return ax
 
 with app.app_context():
-    fig, ax = subplots(
-            figsize=(6.5,5),
-            sharey=True, sharex=True)
+    fig = figure(figsize=(6.5,5))
+    gs = GridSpec(1, 2, width_ratios=[6, 1])
+    ax = fig.add_subplot(gs[0])
+    ax1 = fig.add_subplot(gs[1], sharey=ax)
+
     samples = (Sample.query
         .filter_by(xenolith=True)
         .order_by(Sample.id)
         .all())
 
     for sample in samples:
-        ax = plot_DREE(ax, sample, annotate=False)
+        plot_DREE((ax,ax1), sample, annotate=False)
 
     ax.set_ylabel(u"T (\u00b0C)")
 
