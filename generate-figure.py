64f112f2f9ffcfe8aa65d722712ea4414db0f99c
Daven Quinn <dev@davenquinn.com>
2016-02-29 06:05:06 -0800

    Added some fields as bezier curves

--- generate-figure.py
+++ generate-figure.py
@@ -1,8 +1,10 @@
-from __future__ import division
+from __future__ import division, print_function
 from pandas import DataFrame
 from sys import argv
 import numpy as N
+import matplotlib as M
 from matplotlib import pyplot as P
+from scipy import interpolate
 
 data = DataFrame.from_csv("isotope-data.tsv",sep='\t')
 
@@ -25,6 +27,44 @@ ax.axvline(x=.7045, color="#999999", linestyle="dotted")
 
 # Bulk earth from DePaolo and Wasserburg, 1976
 ax.annotate("Bulk Earth", xy=(.7045,0), xycoords="data", textcoords="offset points", xytext=(5,5))
+
+# Plot splines
+areas = {
+    'continental': '#cccccc',
+    'depleted-mantle': '#aaaaaa',
+    'bulk-earth': '#dddddd',
+    'mantle-array': '#eeeeee'
+}
+
+def spline_interpolate(coords, n=100):
+    nt = N.linspace(0, 1, n)
+    t = N.zeros(coords.shape)
+    t[1:] = N.sqrt((x[1:] - x[:-1])**2 + (y[1:] - y[:-1])**2)
+    t = N.cumsum(t)
+    t /= t[-1]
+    x = [i[0] for i in coords]
+    y = [i[1] for i in coords]
+    x2 = interpolate.spline(t, x, nt)
+    y2 = interpolate.spline(t, y, nt)
+    return [(x,y) for x,y in zip(x2,y2)]
+
+patches = []
+for k,color in areas.items():
+    with open('fields/'+k+'.txt') as f:
+        coords = [i.strip().split() for i in f.readlines()]
+        coords = [(float(a),float(b)) for (a,b) in coords]
+    Path = M.path.Path
+    codes = ([Path.MOVETO]
+             + [Path.CURVE4]*(len(coords)-1))
+
+    path = Path(coords, codes)
+    patch = M.patches.PathPatch(path,
+            facecolor=color,
+            edgecolor='none',
+            zorder = -20 if k == 'mantle-array' else 0)
+    ax.add_artist(patch)
+
+ax.patches = patches
 
 fig.savefig(argv[1], bbox_inches="tight")
 
