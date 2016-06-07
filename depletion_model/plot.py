1d819fd26dc2c9e22009e3f90f58c6b02bac30db
Daven Quinn <dev@davenquinn.com>
2016-06-06 19:05:43 -0700

    Use kwargs interface

--- depletion_model/plot.py
+++ depletion_model/plot.py
@@ -3,12 +3,12 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_pdf import FigureCanvas
 
 @contextlib.contextmanager
-def ree_plot(fn, size=(4,6)):
+def ree_plot(fn, **kwargs):
     """
     Context manager to create plot with appropriate
     axes for plotting REE data
     """
-    fig = Figure(figsize=size)
+    fig = Figure(figsize=kwargs.pop('size',(4,6)))
     fig.canvas = FigureCanvas(fig)
     ax = fig.add_subplot(111)
     ax.set_yscale('log')
